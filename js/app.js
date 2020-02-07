f4f1aaf301f3b206dac3422be3aa439a88097422
tnordberg <tnordberg@smm.org>
2020-02-07 16:54:02 -0500

    Revert keyboard disable
    See:
    https://github.com/scimusmn/jts-0504-spacecraft/commit/6fec556fbc56a79bf7194648607892f310d2088d

--- js/app.js
+++ js/app.js
@@ -17,7 +17,7 @@ require(
   [
     'jquery',
     'net/AppData',
-    'net/Keyboard',
+    // 'net/Keyboard',
     'net/Language',
     'net/ControlManager',
     'net/Sound',
@@ -25,6 +25,7 @@ require(
     'tween'
   ],
   function($, AppData, Keyboard, Language, ControlManager, Sound, TimeTicker, tween) {
+  // function($, AppData, Keyboard, Language, ControlManager, Sound, TimeTicker, tween) {
     /**
      * Initial load
      */
@@ -80,7 +81,7 @@ require(
         ControlManager.setupControls();
 
         // Setup development keyboard commands. For testing only. 
-        Keyboard.init();
+        // Keyboard.init();
 
         // Start space station animation
         startSpaceStationOrbit();
